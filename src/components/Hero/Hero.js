18d4334d3b5eb7f9f539d9adda34257254d772a3
NorthWind <69149199+Amina2000-a@users.noreply.github.com>
2023-07-13 22:27:33 +0400

    add Portfolio website

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+        Welcome to <br />
+        my personal portfolio
+        </SectionTitle>
+        <SectionText>
+        My main goal as a programmer is to create a high-quality, functional, seamless program that takes into account all the needs of the client and achieves maximum convenience in interaction for users. 
+        </SectionText>
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+    In the world of web development, I have worked with a range of technologies from Back-end to Design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <DiReact size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Html<br />
+            Css, Scss, Sass<br />
+            JavaScript<br />
+            Typescript<br />
+            JQuerry<br />
+            React.js<br />
+            Vite.js<br />
+            Tailwind Css<br />
+            Rest APIs<br />
+            React Hooks, Props<br />
+            Bootstrap<br />
+            WordPress<br />
+            Responsibility<br />
+            Git<br />
+            Swagger<br />
+            Postman<br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Node.js<br />
+            Express.js<br />
+            MongoDB<br />
+            Next.js<br />
+            C#<br />
+            .Net (MS/SQL), MVC<br />
+            Asp.NetCore<br />
+            MySql<br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiZend size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Figma<br />
+            Adobe PhotoShop<br />
+            Adobe Illustrator<br />
+            Adobe CoralDraw<br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
+);
+
+export default Technologies;
